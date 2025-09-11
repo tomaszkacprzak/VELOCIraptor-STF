a3af2b63ba431708075f614f6a656e2e658a1f62
tomaszk <tomaszk@phys.ethz.ch>
2025-09-11 19:06:27 +0200

    building up config

--- src/pkdgrav3interface.cxx
+++ src/pkdgrav3interface.cxx
@@ -2,25 +2,56 @@
  *  \brief this file contains stub routines that allow VELOCIraptor to interface with the pkdgrav3 N-body code.
  */
 
- #include "pkdgrav3interface.h"
+#include "pkdgrav3interface.h"
+#include "ioutils.h"
+#include "logging.h"
+#include "timer.h"
 
- #ifdef PKDGRAV3INTERFACE
- 
- Options* pkdgrav3_make_default_options() {
-     // Safe for both aggregates and ctor-based types
-     auto* opt = new Options{};      // value-initialize
-     return opt;
- }
- 
- void pkdgrav3_destroy_options(Options* opt) {
-     delete opt; // legal here: we see the complete type
- }
- 
- void pkdgrav3_load_options(const char* filename, Options &opt) {
-     
-     opt.pname = const_cast<char*>(filename); 
-     fprintf(stdout, "Velociraptor Options->pname: %s\n", opt.pname);
- }
- 
- #endif
- 
+#ifdef PKDGRAV3INTERFACE
+
+
+
+Options* pkdgrav3_make_default_options() {
+    // Safe for both aggregates and ctor-based types
+    auto* opt = new Options{};      // value-initialize
+    return opt;
+}
+
+void pkdgrav3_destroy_options(Options* opt) {
+    delete opt; // legal here: we see the complete type
+}
+
+void pkdgrav3_load_options(const char* filename, Options &opt, const int numthreads) {
+
+    opt.pname = const_cast<char*>(filename); 
+    fprintf(stdout, "Velociraptor Options->pname: %s  numthreads: %d\n", opt.pname, numthreads);
+
+
+    // following swiftinterface.cxx
+
+    //find out how big the SPMD world is
+    MPI_Comm_size(MPI_COMM_WORLD,&NProcs);
+    //mpi_domain=new MPI_Domain[NProcs];
+    mpi_nlocal=new Int_t[NProcs];
+    mpi_nsend=new Int_t[NProcs*NProcs];
+    mpi_ngroups=new Int_t[NProcs];
+    mpi_nhalos=new Int_t[NProcs];
+    //and this processes' rank is
+    MPI_Comm_rank(MPI_COMM_WORLD,&ThisTask);
+    //store MinSize as when using mpi prior to stitching use min of 2;
+    MinNumMPI=2;
+
+    omp_set_num_threads(numthreads);
+
+
+    // logging level is reset correctly later after reading the user configuration
+    vr::init_logging(vr::LogLevel::trace);
+    gsl_set_error_handler_off();
+
+    int iconfigflag;
+
+
+
+}
+
+#endif
--- src/pkdgrav3interface.h
+++ src/pkdgrav3interface.h
@@ -2,19 +2,18 @@
  *  \brief this file contains definitions and routines for interfacing with the pkdgrav3 N-body code.
  */
 
- #ifndef PKDGRAV3INTERFACE_H
- #define PKDGRAV3INTERFACE_H
- 
- #include "allvars.h"
- #include "proto.h"
- 
- #ifdef PKDGRAV3INTERFACE
- 
- Options* pkdgrav3_make_default_options();
- void pkdgrav3_destroy_options(Options* opt);
- void pkdgrav3_load_options(const char* filename, Options &opt);
- 
- #endif
- 
- #endif
- 
+#ifndef PKDGRAV3INTERFACE_H
+#define PKDGRAV3INTERFACE_H
+
+#include "allvars.h"
+#include "proto.h"
+
+#ifdef PKDGRAV3INTERFACE
+
+Options* pkdgrav3_make_default_options();
+void pkdgrav3_destroy_options(Options* opt);
+void pkdgrav3_load_options(const char* filename, Options &opt, const int numthreads);
+
+#endif
+
+#endif
