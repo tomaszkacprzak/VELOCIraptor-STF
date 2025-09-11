fccae9cac0b2faa8ca5e08097c8b72eb359b552e
tomaszk <tomaszk@phys.ethz.ch>
2025-09-11 17:56:22 +0200

    options constr/destr

--- src/pkdgrav3interface.cxx
+++ src/pkdgrav3interface.cxx
@@ -2,20 +2,19 @@
  *  \brief this file contains stub routines that allow VELOCIraptor to interface with the pkdgrav3 N-body code.
  */
 
-#include "pkdgrav3interface.h"
+ #include "pkdgrav3interface.h"
 
-#ifdef PKDGRAV3INTERFACE
-
-int InitVelociraptorPKD()
-{
-    // Placeholder initialisation for pkdgrav3 interface
-    return 0;
-}
-
-int InvokeVelociraptorPKD()
-{
-    // Placeholder invocation for pkdgrav3 interface
-    return 0;
-}
-
-#endif
+ #ifdef PKDGRAV3INTERFACE
+ 
+ Options* make_default_options() {
+     // Safe for both aggregates and ctor-based types
+     auto* o = new Options{};      // value-initialize
+     return o;
+ }
+ 
+ void destroy_options(Options* p) {
+     delete p; // legal here: we see the complete type
+ }
+ 
+ #endif
+ 
--- src/pkdgrav3interface.h
+++ src/pkdgrav3interface.h
@@ -2,18 +2,18 @@
  *  \brief this file contains definitions and routines for interfacing with the pkdgrav3 N-body code.
  */
 
-#ifndef PKDGRAV3INTERFACE_H
-#define PKDGRAV3INTERFACE_H
-
-#include "allvars.h"
-#include "proto.h"
-
-#ifdef PKDGRAV3INTERFACE
-/// Initialise VELOCIraptor when called from pkdgrav3.
-extern "C" int InitVelociraptorPKD();
-
-/// Invoke VELOCIraptor from pkdgrav3.
-extern "C" int InvokeVelociraptorPKD();
-#endif
-
-#endif
+ #ifndef PKDGRAV3INTERFACE_H
+ #define PKDGRAV3INTERFACE_H
+ 
+ #include "allvars.h"
+ #include "proto.h"
+ 
+ #ifdef PKDGRAV3INTERFACE
+ 
+ Options* make_default_options();
+ void destroy_options(Options* p);
+ 
+ #endif
+ 
+ #endif
+ 
